94ebb03e9a777ccc489979b9d68276e149db798c
JGlenn19 <144291521+JGlenn19@users.noreply.github.com>
2023-10-04 15:31:32 -0500

    Add files via upload

---
+++ Deliverables/deliverable.js
@@ -0,0 +1,10 @@
+function calculateSum(n) {
+    let sum = 0;
+  
+    // Loop from 1 to n and add each number to the sum
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+  
+    return sum;
+  }

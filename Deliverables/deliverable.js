2bdf5a0c932fc204b6d02fede2a9149ec66ebe23
JGlenn19 <144291521+JGlenn19@users.noreply.github.com>
2023-10-06 18:17:56 -0500

    Add files via upload

--- Deliverables/deliverable.js
+++ Deliverables/deliverable.js
@@ -1,10 +1,18 @@
-function calculateSum(n) {
-    let sum = 0;
-  
-    // Loop from 1 to n and add each number to the sum
-    for (let i = 1; i <= n; i++) {
-      sum += i;
-    }
-  
-    return sum;
+function calculateSum() {
+  let sum = 0;
+  let n = document.getElementById("oneInput").value;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
   }
+  document.getElementById("oneResult").textContent = sum;
+  return sum;
+};
+
+function printEvenNumbers(n) { 
+    let i = 2;
+     while (i <= n) { console.log(i); i += 2;}
+     
+    console.log("Even numbers up to 10:"); printEvenNumbers(10);
+    console.log("Even numbers up to 20:");
+};
+   
